9c3786a37302f924c0acf9326983c63bafb45542
christoferson <sweetsorrow1981@yahoo.co.jp>
2022-04-11 00:36:05 +0900

    Java 18 - Rename

--- README.md
+++ README.md
@@ -6,3 +6,6 @@ javac --enable-preview --release 18 src/demo/*.java -d bin
 
 ### Run
 java --enable-preview -cp bin demo.TryJava18
+
+
+[JEP 420: Pattern Matching](https://openjdk.java.net/jeps/420)
--- src/demo/TryJava18.java
+++ src/demo/TryJava18.java
@@ -4,15 +4,35 @@ public class TryJava18 {
 
 	public static void main(String[] args) {
 		
-		System.out.println(formatterPatternSwitch(53));
-		System.out.println(formatterPatternSwitch(578L));
+		instanceofPatternMatch(67);
+		instanceofPatternMatch("xyz");
 		
-		testFooBar("Foo");
-		testFooBar("Zoo");
-		testFooBar(null);
+		System.out.println(switchPatternMatch(53));
+		System.out.println(switchPatternMatch(578L));
+		
+		switchNullAndMultiPatternMatch("Foo");
+		switchNullAndMultiPatternMatch("Zoo");
+		switchNullAndMultiPatternMatch(null);
 	}
 	
-	static String formatterPatternSwitch(Object o) {
+	
+	static void instanceofPatternMatch(Object o) {
+		if (o instanceof String) {
+		    String s = (String) o;
+		    System.out.printf("String: %s%n", s);
+		}
+
+		if (o instanceof String s) {
+			System.out.printf("String: %s%n", s);
+		}
+
+		if (o instanceof Integer i) {
+			System.out.printf("Integer: %d%n", i);
+		}
+		
+	}
+	
+	static String switchPatternMatch(Object o) {
 	    return switch (o) {
 	        case Integer i -> String.format("int %d", i);
 	        case Long l    -> String.format("long %d", l);
@@ -22,7 +42,7 @@ public class TryJava18 {
 	    };
 	}
 	
-	static void testFooBar(String s) {
+	static void switchNullAndMultiPatternMatch(String s) {
 	    switch (s) {
 	        case null         -> System.out.println("Oops");
 	        case "Foo", "Bar" -> System.out.println("Great");
